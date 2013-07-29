2165b7e9839360fe2792efc367fbc7ac3a75df56
Giv Parvaneh <giv@givp.org>
2013-07-28 20:27:38 -0700

    Tweaking dist settings

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='RoyGBiv',
-    version='0.2',
+    version='0.3',
     url='https://github.com/givp/RoyGBiv',
     classifiers=[
         'Programming Language :: Python',
         ],
     include_package_data=True,
-    description='A set of image analysis tools',
+    description='A set of image color analysis tools',
     long_description=open("README.md").read(),
     packages=find_packages(),
     author='Giv Parvaneh',
